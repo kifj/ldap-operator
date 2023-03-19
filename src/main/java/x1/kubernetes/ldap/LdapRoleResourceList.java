3432e2053804087231dae23fb63e0afca172d94c
Johannes Beck <mail@johannes-beck.name>
2023-03-19 13:34:13 +0100

    Update Quarkus to 2.16.4

--- src/main/java/x1/kubernetes/ldap/LdapRoleResourceList.java
+++ src/main/java/x1/kubernetes/ldap/LdapRoleResourceList.java
@@ -2,9 +2,9 @@ package x1.kubernetes.ldap;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-import io.fabric8.kubernetes.client.CustomResourceList;
+import io.fabric8.kubernetes.api.model.DefaultKubernetesResourceList;
 
 @JsonSerialize
-public class LdapRoleResourceList extends CustomResourceList<LdapRoleResource> {
+public class LdapRoleResourceList extends DefaultKubernetesResourceList<LdapRoleResource> {
   private static final long serialVersionUID = 1L;
 }
